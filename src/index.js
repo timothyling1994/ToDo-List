824a338b848e562bb1473eb02fea04bd0e9e2e7b
Timothy Ling <timothyling1994@gmail.com>
2021-01-21 10:45:30 -0800

    Add project panel, project view with entries

--- src/index.js
+++ src/index.js
@@ -1,94 +1,203 @@
-const entryFactory = () => {
+/*const entryFactory = () => {
 	
-	let projectName = "";
-	let title = "";
-	let description = "";
-	let dueDate = "";
-	let priority = 1; 
+	let entry = {
+
+		entryId:0,
+		projectId:"",
+		projectName:"",
+		title: "",
+		description: "",
+		dueDate: "",
+		priority:1, 
+
+	};
 
 	const editField = (fieldName,newValue) => {
-		console.log(this);
-		this[fieldName] = newValue;
-		//priority = newValue;	
+		entry[fieldName] = newValue;
 	};
 
 	const returnField = (fieldName) => {
-		console.log(this);
-		return this[fieldName];
-		//console.log(this);
-		//return priority;
+		return entry[fieldName];
 	};
 
-	return {priority, editField, returnField};	
-};
+	return {editField, returnField};	
+};*/
 
-let entry = {
+let projectController = (() => {
 
-	projectName: "",
-	title:"",
-	description: "",
-	dueDate:"",
-	priority:1,
+	//projectArray with projectId has index
+	let projectsCounter = 0;
+	let projectArray = [];
 
-	editField:function(fieldName,newValue){
-		this[fieldName] = newValue;
-	},
-	returnField:function(fieldName){
-		return this[fieldName];
-	}
-}
+	const addtoArray = (projectObj) => {
+
+		projectArray.push(projectObj);
+		projectsCounter+=1; 
+
+	};
+
+	/*const editArray = (projectId, fieldName, newValue) => {
+
+		projectArray[projectId] = project;
+
+	};*/
+
+	const editProject = (projectId, project_name,project_descrip) => {
+		projectArray[projectId].proj_name = project_name;
+		projectArray[projectId].project_descrip = project_descrip;
+		//link DOMController
+	};
+
+	const addNewProject = (project_name,project_descrip) => {
+		let newProj = projectFactory();
+		newProj.proj_name = project_name;
+		newProj.proj_description = project_descrip;
+
+		newProj.projectId = projectsCounter;
+		addtoArray(newProj);
+	};
+
+	const returnProjectsCounter = () => {
+		return projectsCounter;
+	};
+
+	return {addNewProject,returnProjectsCounter,projectArray};
+})();
 
 let projectFactory = () => {
 
+	let projectId = 0;
+	let proj_name = "";
+	let proj_description = "";
+
+	let entryCounter = 0;
+
 	let entryArray = [];
 
-	//let newEntry = Object.assign({},entryFactory());
-	let newEntry = entryFactory();
-	newEntry.editField.call(newEntry);
-	//let newEntry = Object.assign({},entry); //THIS WORKS
-
-	entryArray.push(newEntry);
-
-	const returnEntryArray = () => {
-		return entryArray[0];
+	/*
+	const returnEntryArray = (index) => {
+		return entryArray[index];
 	};
 
-	return {entryArray,returnEntryArray};
+	const addEntry = (entryIdVal, projectIdVal, projectNameVal, titleVal, descriptionVal,dueDateVal,priorityVal) => {
+		
+		let newEntry = entryFactory();
+
+		newEntry.editField("entryId",entryIdVal);
+		newEntry.editField("projectId",projectIdVal);
+		newEntry.editField("projectName",projectNameVal);
+		newEntry.editField("title",titleVal);
+		newEntry.editField("description",descriptionVal);
+		newEntry.editField("dueDate",dueDateVal);
+		newEntry.editField("priority",priorityVal);
+	};
+
+	const editEntry = (entryId, fieldName, newValue) => {
+		
+	};		
+
+	const addtoEntryArray = (entry) => {
+		entryArray.push(newEntry);
+		entryCounter += entryCounter;
+	};*/
+
+	return {entryArray};
 };
 
+let DOMController = (() => {
+
+	let project_panel;
+	let project_entries;
+
+	const init = () => {
+		project_panel = document.querySelector("#project-panel");
+		project_entries = document.querySelector("#project-entries");
+
+		let addModal = document.getElementById("modalWindow");
+
+		let add_proj_btn = document.querySelector("#add-project");
+
+		add_proj_btn.addEventListener("click",function(){
+			addModal.style.display = "block";
+		});
+
+		let modal_close_btn = document.getElementsByClassName("close")[0];
+
+		modal_close_btn.addEventListener("click",function(){
+			addModal.style.display = "none";
+		});
+
+		let add_submit_btn = document.querySelector("#submit");
+		add_submit_btn.addEventListener("click",function(){
+
+			let proj_name = document.querySelector("#project-name-input").value;
+ 			let proj_descrip = document.querySelector("#project-descrip-input").value;
+
+			projectController.addNewProject(proj_name,proj_descrip);
+			let id = projectController.returnProjectsCounter() - 1;
+			addNewProjectDOM(proj_name,proj_descrip,id);
+
+			addModal.style.display = "none";
+		});
+
+	}
+
+	const addNewProjectDOM = (project_name, project_descrip, project_id) => {
+
+		let newProjectDiv =  document.createElement("div");
+		newProjectDiv.setAttribute("id","project-"+project_id);
+		newProjectDiv.classList.add("project");
+
+		let projectInfo = document.createElement("div");
+		projectInfo.setAttribute("id","project-info");
 
 
-let firstProject = projectFactory();
+		let nameDiv =  document.createElement("div");
+		let nameTextNode = document.createTextNode(project_name);
+		nameDiv.appendChild(nameTextNode);
+		nameDiv.classList.add("project-name")
+
+		let descriptionDiv =  document.createElement("div");
+		let descripTextNode = document.createTextNode(project_descrip);
+		descriptionDiv.appendChild(descripTextNode);
+		descriptionDiv.classList.add("project-description")
+
+		projectInfo.appendChild(nameDiv,projectInfo);
+		projectInfo.appendChild(descriptionDiv,projectInfo);
+		newProjectDiv.appendChild(projectInfo,newProjectDiv);
 
 
-/*
+		let projectBtns = document.createElement("div");
+		projectBtns.setAttribute("id","project-btns-container");
 
-let firstProject = projectFactory();
-let secondProject = projectFactory();
+		let editBtn = document.createElement("div");
+		editBtn.classList.add("project-btns")
+		let editText = document.createTextNode("Edit");
+		editBtn.appendChild(editText);
 
-//let newEntry1 = Object.assign({},entry); //THIS WORKS
-let newEntry1 = Object.assign({},entryFactory()); 
+		let deleteBtn = document.createElement("div");
+		deleteBtn.classList.add("project-btns")
+		let deleteText = document.createTextNode("Delete");
+		deleteBtn.appendChild(deleteText);
 
-newEntry1.editField("priority",9);
-//console.log("project_name:" + newEntry1.priority);
-console.log(newEntry1.returnField.call(newEntry1,"priority"));
-
-newEntry1.editField("priority",10);
-console.log(newEntry1.returnField.call(newEntry1,"priority"));
-
-//console.log(firstProject.returnEntryArray().returnField("priority"));
-//console.log(firstProject.returnEntryArray().returnField.call(entryFactory,"priority"));
-//firstProject.returnEntryArray().editField.call(entryFactory,"priority",1000);
-//console.log(firstProject.returnEntryArray().returnField.call(entryFactory,"priority"));
-
-//console.log(newEntry1.returnField("priority"));
-//newEntry1.editField("priority",666);
-//console.log(newEntry1.returnField("priority"));
-
-//console.log(secondProject.returnEntryArray().returnField.call(entryFactory,"priority"));
+		projectBtns.appendChild(editBtn,projectBtns);
+		projectBtns.appendChild(deleteBtn,projectBtns);
+		newProjectDiv.appendChild(projectBtns,newProjectDiv);
 
 
-*/
+		project_entries.appendChild(newProjectDiv,project_entries);
 
+	};
+
+	return{init};
+
+})();
+
+
+function theDomHasLoaded(e) {
+	DOMController.init();
+}
+
+document.addEventListener("DOMContentLoaded",theDomHasLoaded,false);
 
 
