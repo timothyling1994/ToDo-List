1d8598cfada64873850173d458c016843360c336
Timothy Ling <timothyling1994@gmail.com>
2022-01-12 15:42:20 -0800

    Deploy

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
     watchContentBase: true,
   },
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
   },
   module: {  
     rules: [  
